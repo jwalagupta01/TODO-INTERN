ed79b4dedfc8a07bfe261649f327872969b96b69
Jwala Gupta <jwalagupta58@gmail.com>
2026-03-25 11:00:55 +0530

    Update dependencies and remove unused user mock data

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./Context/Context.jsx";
+// import { store } from "./redux/store.js";
+// import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    {/* <Provider store={store}> */}
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    {/* </Provider> */}
   </BrowserRouter>,
 );
